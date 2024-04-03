2e47722d3f9bcd09118cce7c5ab0c283498c6a37
diegolr-avanade <diego.lopez.reduello@avanade.com>
2024-04-03 12:44:54 +0200

    ✨Module4 finished

--- Controlling-Program-Flow.Reports/Program.cs
+++ Controlling-Program-Flow.Reports/Program.cs
@@ -1,14 +1,69 @@
-﻿using System;
+﻿using ControllingProgramFlow.Reports;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
-namespace WiredBrainCoffeeSurveys.Reports
+namespace ControllingProgramFlow.Reports
 {
     class Program
     {
         static void Main(string[] args)
         {
+            GenerateWinnerEmails();
+
             GenerateTasksReport();
+
+            GenerateCommentsReport();
+        }
+
+        public static void GenerateWinnerEmails()
+        {
+            var selectedEmails = new List<string>();
+            int counter = 0;
+
+            Console.WriteLine(Environment.NewLine + "Selected Winners Output:");
+            while (selectedEmails.Count < 2 && counter < Q1Results.Responses.Count)
+            {
+                var currentItem = Q1Results.Responses[counter];
+
+                if (currentItem.FavoriteProduct == Q1Results.FavoriteProduct)
+                {
+                    selectedEmails.Add(currentItem.EmailAddress);
+                    Console.WriteLine(currentItem.EmailAddress);
+                }
+
+                counter++;
+            }
+
+            File.WriteAllLines("WinnersReport.csv", selectedEmails);
+        }
+
+        public static void GenerateCommentsReport()
+        {
+            var comments = new List<string>();
+
+            Console.WriteLine(Environment.NewLine + "Comments Output:");
+            for (var i = 0; i < Q1Results.Responses.Count; i++)
+            {
+                var currentResponse = Q1Results.Responses[i];
+
+                if (currentResponse.WouldRecommend < 7.0)
+                {
+                    Console.WriteLine(currentResponse.Comments);
+                    comments.Add(currentResponse.Comments);
+                }
+            }
+
+            foreach (var response in Q1Results.Responses)
+            {
+                if (response.AreaToImprove == Q1Results.AreaToImprove)
+                {
+                    Console.WriteLine(response.Comments);
+                    comments.Add(response.Comments);
+                }
+            }
+
+            File.WriteAllLines("CommentsReport.csv", comments);
         }
 
         public static void GenerateTasksReport()
@@ -60,6 +115,15 @@ namespace WiredBrainCoffeeSurveys.Reports
                     tasks.Add("Investigate individual comments for ideas.");
                     break;
             }
+
+            Console.WriteLine(Environment.NewLine + "Tasks Output:");
+            foreach (var task in tasks)
+            {
+                Console.WriteLine(task);
+            }
+
+            File.WriteAllLines("TasksReport.csv", tasks);
+            Console.ReadLine();
         }
     }
 }
