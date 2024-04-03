45e4b7d1c4e4d911b66790bf6350edeb68287943
diegolr-avanade <diego.lopez.reduello@avanade.com>
2024-04-03 12:10:43 +0200

    ✨Start module 2

---
+++ Controlling-Program-Flow.Reports/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WiredBrainCoffeeSurveys.Reports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
