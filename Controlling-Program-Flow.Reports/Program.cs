df7a1821d2de725b99add9cc8b529f421ebe2dee
diegolr-avanade <diego.lopez.reduello@avanade.com>
2024-04-03 13:06:27 +0200

    ✨Module 5

--- Controlling-Program-Flow.Reports/Program.cs
+++ Controlling-Program-Flow.Reports/Program.cs
@@ -1,4 +1,4 @@
-﻿using ControllingProgramFlow.Reports;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,24 +9,59 @@ namespace ControllingProgramFlow.Reports
     {
         static void Main(string[] args)
         {
-            GenerateWinnerEmails();
+            bool quitApp = false;
 
-            GenerateTasksReport();
+            do
+            {
+                Console.WriteLine("Please specify a report to run (rewards, comments, tasks, quit):");
+                var selectedReport = Console.ReadLine();
 
-            GenerateCommentsReport();
+                if (selectedReport == "quit")
+                {
+                    quitApp = true;
+                }
+                else
+                {
+                    Console.WriteLine("Please specify which quarter of data: (q1, q2)");
+                    var selectedData = Console.ReadLine();
+
+                    var surveyResults = JsonConvert.DeserializeObject<SurveyResults>
+                        (File.ReadAllText($"data/{selectedData}.json"));
+
+                    switch (selectedReport)
+                    {
+                        case "rewards":
+                            GenerateWinnerEmails(surveyResults);
+                            break;
+                        case "comments":
+                            GenerateCommentsReport(surveyResults);
+                            break;
+                        case "tasks":
+                            GenerateTasksReport(surveyResults);
+                            break;
+                        default:
+                            Console.WriteLine("Sorry, that's not a valid option.");
+                            break;
+                    }
+                }
+
+                Console.WriteLine();
+            }
+            while (!quitApp);
+            Console.ReadLine();
         }
 
-        public static void GenerateWinnerEmails()
+        public static void GenerateWinnerEmails(SurveyResults results)
         {
             var selectedEmails = new List<string>();
             int counter = 0;
 
             Console.WriteLine(Environment.NewLine + "Selected Winners Output:");
-            while (selectedEmails.Count < 2 && counter < Q1Results.Responses.Count)
+            while (selectedEmails.Count < 2 && counter < results.Responses.Count)
             {
-                var currentItem = Q1Results.Responses[counter];
+                var currentItem = results.Responses[counter];
 
-                if (currentItem.FavoriteProduct == Q1Results.FavoriteProduct)
+                if (currentItem.FavoriteProduct == "Cappucino")
                 {
                     selectedEmails.Add(currentItem.EmailAddress);
                     Console.WriteLine(currentItem.EmailAddress);
@@ -38,14 +73,14 @@ namespace ControllingProgramFlow.Reports
             File.WriteAllLines("WinnersReport.csv", selectedEmails);
         }
 
-        public static void GenerateCommentsReport()
+        public static void GenerateCommentsReport(SurveyResults results)
         {
             var comments = new List<string>();
 
             Console.WriteLine(Environment.NewLine + "Comments Output:");
-            for (var i = 0; i < Q1Results.Responses.Count; i++)
+            for (var i = 0; i < results.Responses.Count; i++)
             {
-                var currentResponse = Q1Results.Responses[i];
+                var currentResponse = results.Responses[i];
 
                 if (currentResponse.WouldRecommend < 7.0)
                 {
@@ -54,9 +89,9 @@ namespace ControllingProgramFlow.Reports
                 }
             }
 
-            foreach (var response in Q1Results.Responses)
+            foreach (var response in results.Responses)
             {
-                if (response.AreaToImprove == Q1Results.AreaToImprove)
+                if (response.AreaToImprove == results.AreaToImprove)
                 {
                     Console.WriteLine(response.Comments);
                     comments.Add(response.Comments);
@@ -66,26 +101,17 @@ namespace ControllingProgramFlow.Reports
             File.WriteAllLines("CommentsReport.csv", comments);
         }
 
-        public static void GenerateTasksReport()
+        public static void GenerateTasksReport(SurveyResults results)
         {
             var tasks = new List<string>();
 
-            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
-            double overallScore = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
+            double responseRate = results.NumberResponded / results.NumberSurveyed;
+            double overallScore = (results.ServiceScore + results.CoffeeScore + results.FoodScore + results.PriceScore) / 4;
 
-            if (Q1Results.CoffeeScore < Q1Results.FoodScore)
-            {
+            if (results.CoffeeScore < results.FoodScore)
                 tasks.Add("Investigate coffee recipes and ingredients.");
-            }
 
-            if (overallScore > 8.0)
-            {
-                tasks.Add("Work with leadership to reward staff");
-            }
-            else
-            {
-                tasks.Add("Work with employees for improvement ideas.");
-            }
+            tasks.Add(overallScore > 8.0 ? "Work with leadership." : "Work with employees for ideas.");
 
             if (responseRate < .33)
             {
@@ -100,21 +126,13 @@ namespace ControllingProgramFlow.Reports
                 tasks.Add("Rewards participants with discount coffee coupon.");
             }
 
-            switch (Q1Results.AreaToImprove)
+            tasks.Add(results.AreaToImprove switch
             {
-                case "RewardsProgram":
-                    tasks.Add("Revisit the rewards deals.");
-                    break;
-                case "Cleanliness":
-                    tasks.Add("Contact the cleaning vendor.");
-                    break;
-                case "MobileApp":
-                    tasks.Add("Contact the consulting firm about app.");
-                    break;
-                default:
-                    tasks.Add("Investigate individual comments for ideas.");
-                    break;
-            }
+                "RewardsProgram" => "Revisit the rewards deals.",
+                "Cleanliness" => "Contact the cleaning vendor",
+                "MobileApp" => "Contact the consulting firm about the app.",
+                _ => "Investigate individual comments for ideas."
+            });
 
             Console.WriteLine(Environment.NewLine + "Tasks Output:");
             foreach (var task in tasks)
@@ -123,7 +141,6 @@ namespace ControllingProgramFlow.Reports
             }
 
             File.WriteAllLines("TasksReport.csv", tasks);
-            Console.ReadLine();
         }
     }
 }
